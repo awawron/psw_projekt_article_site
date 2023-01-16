c8a1e8d217211dfb0954c023b3aa1752e1574756
a-wawron <wawron.adam@gmail.com>
2023-01-16 19:42:00 +0100

    Basic server functions for home and login pages

--- Frontend/src/components/Main/HomePage.js
+++ Frontend/src/components/Main/HomePage.js
@@ -1,34 +1,37 @@
 import { useEffect, useState } from "react";
 import ArticlePreview from "../Article/ArticlePreview"
+import axios from "axios"
 
 const HomePage = () => {
-    const [ids, setIds] = useState([])
+    const [articles, setArticles] = useState([])
 
-    // get a list of 6 random ids for the previews
     useEffect(() => {
-        // n = 6
-        // axios.get(`/randomids/${n}`)
-        //   .then(res => res.json())
-        //   .then(data => setIds(data));
-      }, []);
+        console.log("Go get articles")
+        axios.get("/home")
+            .then(res => {
+                setArticles(res.data)
+        })
+    }, []);
+    
 
     return (<div>
         <h1>Welcome to ArticleSuperSite!</h1>
-        {/* <table className="preview-table">
-            <tr>
-                <td><div className="article-preview"><ArticlePreview id={ids[0]}/></div></td>
-                <td><div className="article-preview"><ArticlePreview id={ids[1]}/></div></td>
-            </tr>
-            <tr>
-                <td><div className="article-preview"><ArticlePreview id={ids[2]}/></div></td>
-                <td><div className="article-preview"><ArticlePreview id={ids[3]}/></div></td>
-            </tr>
-            <tr>
-                <td><div className="article-preview"><ArticlePreview id={ids[4]}/></div></td>
-                <td><div className="article-preview"><ArticlePreview id={ids[5]}/></div></td>
-            </tr>
-        </table> */}
-        <h2>Table with previews here</h2>
+        <table className="preview-table">
+            <tbody>
+                <tr>
+                    <td className="article-preview"><ArticlePreview a={articles[ 1 % articles.length ]}/></td>
+                    <td className="article-preview"><ArticlePreview a={articles[ 2 % articles.length ]}/></td>
+                </tr>
+                <tr>
+                    <td className="article-preview"><ArticlePreview a={articles[ 3 % articles.length ]}/></td>
+                    <td className="article-preview"><ArticlePreview a={articles[ 4 % articles.length ]}/></td>
+                </tr>
+                <tr>
+                    <td className="article-preview"><ArticlePreview a={articles[ 5 % articles.length ]}/></td>
+                    <td className="article-preview"><ArticlePreview a={articles[ 6 % articles.length ]}/></td>
+                </tr>
+            </tbody>
+        </table>
     </div>)
 }
 
