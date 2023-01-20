13ed086612bd717b99a0a3e4e5f2b108e01200a7
a-wawron <wawron.adam@gmail.com>
2023-01-20 16:47:26 +0100

    Login and profile page

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
-const userFunctions = require("./functions/authFunctions")
+const authFunctions = require("./functions/authFunctions")
 const articleFunctions = require("./functions/articleFunctions")
+const userFunctions = require("./functions/userFunctions")
 
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const { profile } = require("console");
 
 const app = express();
 app.use(express.json())
@@ -19,7 +21,7 @@ app.get("/home", (req, res) => {
 app.post("/login", (req, res) => {
     console.log(req.body)
     const { username, password } = req.body;
-    const verified = userFunctions.verifyUser(username, password)
+    const verified = authFunctions.verifyUser(username, password)
     if(verified === true) {
         res.cookie('user', username, {
             maxAge: 1800000, // 30 minutes in milliseconds
@@ -37,6 +39,13 @@ app.get("/logout", (req, res) => {
     console.log("logout initiated")
     res.clearCookie('user')
     res.send({ message: "Logged out" })
+})
+
+app.get("/profile:username", (req, res) => {
+    const username = req.params.username
+    console.log("Getting profile of: " + username)
+    const profile = userFunctions.getUserByUsername(username)
+    res.send({username: profile.username, email: profile.email, password: profile.password, clearance: profile.clearance})
 })
 
 console.log(`Server listening at port ${port}`);
--- Frontend/src/components/Profile/ProfilePage.js
+++ Frontend/src/components/Profile/ProfilePage.js
@@ -1,20 +1,69 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import Navbar from "../Main/Navbar";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 const ProfilePage = () => {
+    const navigate = useNavigate()
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null)
+    const [profile, setProfile] = useState({username: 'Loading...', email: 'Loading...', password: 'Loading...', clearance: 'Loading...'})
+
+    const getClearance = (number) => {
+        switch (number) {
+            case 0:
+                return "Normal user";
+            case 1:
+                return "Author";
+            case 2:
+                return "Admin";
+            default:
+                return "Loading";
+        }
+    }
 
     useEffect(() => {
-        // TODO
-        // Check the cookie for if the user is logged in
-        // If not redirect to login
+        const checkUser = async () => {
+            const cook = Cookies.get('user')
+            return cook
+        }
+
+        const fetchProfile = async (username) => {
+            const profile = axios.get(`/profile${username}`)
+            return profile
+        }
+
+        checkUser().then(us => {
+            if (us !== undefined) {
+                setUser(us)
+                setIsLoggedIn(true)
+                fetchProfile(us).then(prof => setProfile(prof.data))
+            }
+            else {
+                navigate('/login')
+            }
+        })
     }, []);
 
     return (
         <div>
             <Navbar />
-            <h2>Your profile here</h2>
+            <div className="page">
+                {isLoggedIn ? (
+                    <div>
+                        <h2>Welcome {user}!</h2>
+                        <div>
+                            <div>Username : {profile.username}</div>
+                            <div>Email    : {profile.email}</div>
+                            <div>Password : {profile.password}</div>
+                            <div>User type: {getClearance(profile.clearance)}</div>
+                        </div>
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
         </div>
     )
 }
