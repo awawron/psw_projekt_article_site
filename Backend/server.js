98119928a188e641d83b5af498929cb194b1f934
awawron <a.wawron.693@studms.ug.edu.pl>
2023-01-26 18:35:46 +0100

    Better formatting

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,41 @@ app.get("/articles", (req, res) => {
     res.send(articles)
 })
 
+// Clear the cookie responsible for login
+app.get("/logout", (req, res) => {
+    console.log("logout initiated")
+    res.clearCookie('user')
+    res.send({ message: "Logged out" })
+})
+
+// Find and send the user data by username
+app.get("/profile:username", (req, res) => {
+    const username = otherFunctions.unHashString(req.params.username)
+    console.log("Getting profile of: " + username)
+    const profile = userFunctions.getUserByUsername(username)
+    res.send({username: profile.username, email: profile.email, password: profile.password, clearance: profile.clearance})
+})
+
+// Find and send the article with given id
+app.get("/article:id", (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const article = articleFunctions.getArticleById(id)
+    res.send(article)
+})
+
+app.get("/users", (req, res) => {
+    const users = userFunctions.getUsers()
+    res.send(users)
+})
+
+app.get('/search', (req, res) => {
+    const { query } = req.query;
+    const filteredArticles = articleFunctions.searchArticles(query)
+  
+    res.send(filteredArticles);
+});
+
 // When the user tries to log in check the password and set a cookie if it matches
 app.post("/login", (req, res) => {
     console.log(req.body)
@@ -36,19 +71,9 @@ app.post("/login", (req, res) => {
     }    
 });
 
-// Clear the cookie responsible for login
-app.get("/logout", (req, res) => {
-    console.log("logout initiated")
-    res.clearCookie('user')
-    res.send({ message: "Logged out" })
-})
-
-// Find and send the user data by username
-app.get("/profile:username", (req, res) => {
-    const username = otherFunctions.unHashString(req.params.username)
-    console.log("Getting profile of: " + username)
-    const profile = userFunctions.getUserByUsername(username)
-    res.send({username: profile.username, email: profile.email, password: profile.password, clearance: profile.clearance})
+app.post("/articles", (req, res) => {
+    const id = articleFunctions.createArticle(req.body)
+    res.send('Article created with an id ' + id)
 })
 
 // Change password
@@ -82,30 +107,7 @@ app.delete("/article:id", (req, res) => {
     res.send('Article ' + id + ' removed')
 })
 
-// Find and send the article with given id
-app.get("/article:id", (req, res) => {
-    const id = req.params.id
-    console.log(id)
-    const article = articleFunctions.getArticleById(id)
-    res.send(article)
-})
 
-app.get("/users", (req, res) => {
-    const users = userFunctions.getUsers()
-    res.send(users)
-})
-
-app.post("/articles", (req, res) => {
-    const id = articleFunctions.createArticle(req.body)
-    res.send('Article created with an id ' + id)
-})
-
-app.get('/search', (req, res) => {
-    const { query } = req.query;
-    const filteredArticles = articleFunctions.searchArticles(query)
-  
-    res.send(filteredArticles);
-});
 
 console.log(`Server listening at port ${port}`);
 app.listen(port);
